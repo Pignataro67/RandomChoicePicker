baabb7addd4968cb9e07da479b5717dfc666a5f4
Patrick <Pignataro67@gmail.com>
2025-05-18 06:46:09 -0400

    CSS

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } 
+    }
