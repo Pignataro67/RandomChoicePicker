c885bd5eb12280e870415d7471fde07a0432a3ad
Patrick <Pignataro67@gmail.com>
2025-05-25 06:48:45 -0400

    JS

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ let isPressed = false
 colorEl.value = 'black'
 let color = colorEl.value
 let x
+let y
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+}
