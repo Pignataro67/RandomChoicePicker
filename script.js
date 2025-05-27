9901884f96826dc2ccb3d6d7231d0eaa2905481e
Patrick <Pignataro67@gmail.com>
2025-05-27 09:13:01 -0400

    CSS

--- script.js
+++ script.js
@@ -44,4 +44,6 @@ canvas.addEventListener('mousemove', (e) => {
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
 }
