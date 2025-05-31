2e9e810d355f747f6d425b2924793480b50c0eea
Patrick <Pignataro67@gmail.com>
2025-05-31 12:58:31 -0400

    JS

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ window.addEventListener('keydown', (event) => {
   insert.innerHTML = `
   <div class="key">
   ${event.key === ' ' ? 'Space' : event.key} 
-  `
+  <small>event.key</small>
+</div>
+`
 })
