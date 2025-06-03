53298708b9b80ab0dd85e71d6259003583a7879d
Patrick <Pignataro67@gmail.com>
2025-06-03 11:06:52 -0400

    JS

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const panels = document.querySelectorAll('.panel')
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
+        panel.classList.add('active')
+    })
 })
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+)}}
