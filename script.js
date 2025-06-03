07c8b2d0d6c5e358c4ef0f353343d37d845680cc
Patrick <Pignataro67@gmail.com>
2025-06-03 11:05:55 -0400

    CSS

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-const insert = document.getElementById('insert')
+const panels = document.querySelectorAll('.panel')
 
-window.addEventListener('keydown', (event) => {
-  insert.innerHTML = `
-  <div class="key">
-  ${event.key === ' ' ? 'Space' : event.key} 
-  <small>event.key</small>
-</div>
-
-<div class="key">
-  ${event.keyCode}
-  <small>event.keyCode</small>
-</div>
-
-<div class="key">
-  ${event.code}
-  <small>event.code</small>
-</div>
-`
+panels.forEach(panel => {
+    
 })
