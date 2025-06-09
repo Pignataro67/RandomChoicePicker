3ec7d93e9d91b4d8bbd5c328356e293f19b1e3d6
Patrick <Pignataro67@gmail.com>
2025-06-09 09:59:52 -0400

    CSS

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const panels = document.querySelectorAll('.panel')
-
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
     })
 })
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
