06c85455a05d0d46ec43fa426c887ab29f9b957c
Patrick <Pignataro67@gmail.com>
2025-06-09 10:00:11 -0400

    JS

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const toggles = document.querySelectorAll('.faq-toggle')
 
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
     })
 })
