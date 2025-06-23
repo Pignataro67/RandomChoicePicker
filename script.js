ef03c6b67f90b08a6c245ec0d634a2ffd0c45632
Patrick <Pignataro67@gmail.com>
2025-06-23 08:39:50 -0400

    CSS

--- script.js
+++ script.js
@@ -18,5 +18,11 @@ async function getUser(username) {
 }
 
 async function getRepos(username) {
-    
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
+
+        addReposToCard(data)
+    } catch(err) {
+        createErrorCard('Problem fetching repos')
+    }
 }
