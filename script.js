a0a4f2cbea380afc503e97175fa084c85a43c34a
Patrick <Pignataro67@gmail.com>
2025-06-28 08:20:51 -0400

    JS

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const cheap = document.querySelector('#cheap')
 const fast = document.querySelector('#fast')
 
 toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickedOne) {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === theClickedOne) {
+            fast.checked = false
+        }
+}
