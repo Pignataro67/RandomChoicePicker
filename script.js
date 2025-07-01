4baab2e16375fc3988b85be9845d261dc50c8745
Patrick <Pignataro67@gmail.com>
2025-07-01 13:23:10 -0400

    JS

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
 btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
 })
