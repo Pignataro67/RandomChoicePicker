39a18144be193759da44fdf2507fd8b1156c28a9
Patrick <Pignataro67@gmail.com>
2025-07-06 21:28:25 -0400

    JS

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const imgs = document.getElementById('imgs')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
+}
