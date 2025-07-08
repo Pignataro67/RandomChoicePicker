1be729457d9cba2f33ad33bfd391de34a937c1ec
Patrick <Pignataro67@gmail.com>
2025-07-08 09:41:12 -0400

    JS

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-const imgs = document.getElementById('imgs')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
+const counters = document.querySelectorAll('.counter')
 
-const img = document.querySelectorAll('#imgs img')
+counters.forEach(counter => {
+    counter.innerText = '0'
 
-let idx = 0
-
-let interval = setInterval(run, 2000)
-
-function run() {
-    idx++
-    changeImage()
-}
-
-function changeImage() {
-    if(idx > img.length - 1) {
-        idx = 0
-    } else if(idx < 0) {
-        idx = img.length - 1
-    }
-
-    imgs.style.transform = `translateX(${-idx * 500}px)`
-}
-
-function resetInterval() {
-    clearInterval(interval)
-    interval = setInterval(run, 2000)
-}
-
-rightBtn.addEventListener('click', () => {
-    idx++
-    changeImage()
-    resetInterval()
-})
-
-leftBtn.addEventListener('click', () => {
-    idx--
-    changeImage()
-    resetInterval()
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
 })
