e7774b784afbef091aa21f197dd934425f0eb7bb
Patrick <Pignataro67@gmail.com>
2025-07-08 09:41:22 -0400

    JS

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ counters.forEach(counter => {
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 200
 })
