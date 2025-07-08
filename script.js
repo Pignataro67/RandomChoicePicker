e421c04d5dc149225e6163b032e8a3642e127418
Patrick <Pignataro67@gmail.com>
2025-07-08 09:41:32 -0400

    JS

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ counters.forEach(counter => {
         const c = +counter.innerText
 
         const increment = target / 200
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
 })
