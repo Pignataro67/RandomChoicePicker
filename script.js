a63ba49f599fff8a38fe798fcf8975f843beee82
Patrick <Pignataro67@gmail.com>
2025-07-14 21:25:59 -0400

    JS

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-const counters = document.querySelectorAll('.counter')
-
-counters.forEach(counter => {
-    counter.innerText = '0'
-
-    const updateCounter = () => {
-        const target = +counter.getAttribute('data-target')
-        const c = +counter.innerText
-
-        const increment = target / 200
-
-        if(c < target) {
-            counter.innerText = `${Math.ceil(c + increment)}`
-            setTimeout(updateCounter, 1)
-        } else {
-            counter.innerText = target
-        }
-    }
-
-    updateCounter()
-})
+const screens = document.querySelectorAll('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn')
+const game_container = document.getElementById('game-container')
