9bb307b4578c4e2b39be60f406318e109719903d
Patrick <Pignataro67@gmail.com>
2025-07-23 08:38:04 -0400

    JS

--- script.js
+++ script.js
@@ -7,5 +7,11 @@ getData()
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
 async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=50')
+
+    const { results } = await res.json()
+
+    // Clear result
+    result.innerHTML = ''
 
 }
