7fea8228ed7a7ea9ed78f5f19bb3455a9e195025
Patrick <Pignataro67@gmail.com>
2025-07-23 08:37:37 -0400

    JS

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+const listItems = []
+
+getData()
+
 filter.addEventListener('input', (e) => filterData(e.target.value))
 
 async function getData() {
