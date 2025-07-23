ff32c7cb2630550bffe6a4e554e226921a0c4fa1
Patrick <Pignataro67@gmail.com>
2025-07-23 08:39:08 -0400

    JS

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ async function getData() {
         result.appendChild(li)
     })
 }
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+        }
+    }
+}
