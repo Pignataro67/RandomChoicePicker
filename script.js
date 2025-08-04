cb54240f42494a16ea455bc5aa62caeb5592ac36
Patrick <Pignataro67@gmail.com>
2025-08-03 20:26:26 -0400

    JS

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-const contents = document.querySelectorAll('.content')
-const listItems = document.querySelectorAll('nav ul li')
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
-listItems.forEach((item, idx) => {
-    item.addEventListener('click', () => {
-        hideAllContents()
-        hideAllItems()
-
-        item.classList.add('active')
-        contents[idx].classList.add('show')
-    })
-})
-
-function hideAllContents() {
-    contents.forEach(content => content.classList.remove('show'))
-}
-
-function hideAllItems() {
-    listItems.forEach(item => item.classList.remove('active'))
-}
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
