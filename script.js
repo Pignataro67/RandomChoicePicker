50c95ffdf7cd33af575b178f5753b07ee2355a70
Patrick <Pignataro67@gmail.com>
2025-08-03 20:27:24 -0400

    JS

--- script.js
+++ script.js
@@ -20,5 +20,11 @@ function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) => {
-    
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+
 }
