8af0f6028e8b0dce654ddc8f5542e3c974d4d9f7
Patrick <Pignataro67@gmail.com>
2025-08-17 09:59:50 -0400

    JS

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const password = document.getElementById('password')
-const background = document.getElementById('background')
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
-password.addEventListener('input', (e) => {
-  const input = e.target.value
-  const length = input.length
-  const blurValue = 20 - length * 2
-  background.style.filter = `blur(${blurValue}px)`
+let currentActive = 1
+
+next.addEventListener('click', () => {
+   
 })
