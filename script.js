6d413149f1ada1ba4cd7007d284ab2a391a407ef
Patrick <Pignataro67@gmail.com>
2025-08-23 10:10:00 -0400

    JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+];
