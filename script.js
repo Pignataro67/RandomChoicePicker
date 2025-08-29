fdc1a6eb4c9f57afe8ecbb44bcfe7f6f8ce10106
Patrick <Pignataro67@gmail.com>
2025-08-29 08:07:17 -0400

    JS

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ const textarea = document.getElementById('textarea')
 textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
-   
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
+ 
