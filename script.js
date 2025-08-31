4199f80524cfb2209326116ebaaababa714304ca
Patrick <Pignataro67@gmail.com>
2025-08-30 21:41:28 -0400

    JS

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ function randomSelect() {
     }, 100);
 
     setTimeout(() => {
-        
-    })
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+
+    }, times * 100)
 }
