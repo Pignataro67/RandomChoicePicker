b4cafa279a71b31440f1649c125e59706568d094
Patrick <Pignataro67@gmail.com>
2025-09-20 09:12:19 -0400

    JS

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const resetBtn = document.querySelector('#reset');
 const playBtn = document.querySelector('#play');
 const timerEl = document.querySelector('#timer');
 const root = document.querySelector(':root');
+
+// Initial setup
+const totalSeconds = 60;
+let playing = false;
+let currentSeconds = totalSeconds;
+timerEl.innerText = formatTime(totalSeconds);
