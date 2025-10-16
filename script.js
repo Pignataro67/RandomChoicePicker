e5444b5eddc56f355550fdcce020ed4294fac978
Patrick <Pignataro67@gmail.com>
2025-10-16 08:02:48 -0400

    JS

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const password = document.getElementById('password')
-const background = document.getElementById('background')
+const testimonials = [
+  {
+    name: 'Miyah Myles',
+    position: 'Marketing',
+    photo:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
+    text:
+      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+  },
+]
 
-password.addEventListener('input', (e) => {
-  const input = e.target.value
-  const length = input.length
-  const blurValue = 20 - length * 2
-})
+let idx = 1
 
 function updateTestimonial() {
   const { name, position, photo, text } = testimonials[idx]
