1930998cf9577f8068ca796cffc7926fc52435da
Patrick <Pignataro67@gmail.com>
2025-10-16 08:03:47 -0400

    JS

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const testimonialsContainer = document.querySelector('.testimonials-container')
+const testimonial = document.querySelector('.testimonial')
+const userImage = document.querySelector('.user-image')
+const username = document.querySelector('.username')
+const role = document.querySelector('.role')
+
 const testimonials = [
   {
     name: 'Miyah Myles',
