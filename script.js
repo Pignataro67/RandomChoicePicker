e2e94339314a296b4c39f0a43ce325f298f96272
Patrick <Pignataro67@gmail.com>
2025-10-25 08:39:16 -0400

    JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+
+})
+
+function setTime() {
+   
+}
