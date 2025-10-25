c52fd77fc82736f6fdae3ac8195a45bb34e8ff65
Patrick <Pignataro67@gmail.com>
2025-10-25 08:39:26 -0400

    JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
 
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
