a5ed8bc356cc49f17d68e25e5e0292cceab943c4
Patrick <Pignataro67@gmail.com>
2025-11-04 10:12:50 -0500

    JS

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const todosUL = document.getElementById('todos')
 const todos = JSON.parse(localStorage.getItem('todos'))
 
 if(todos) {
+    todos.forEach(todo => addTodo(todo))
 }
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
 })
