a8bdcd4023580c9d38bf098c58c8e1976ef15a71
Patrick <Pignataro67@gmail.com>
2025-11-04 10:12:24 -0500

    JS

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-const button = document.getElementById('button')
-const toasts = document.getElementById('toasts')
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos')
 
-const messages = [
-    'Message One',
-    'Message Two',
-    'Message Three',
-    'Message Four',
-]
-
-const types = ['info', 'success', 'error']
-
-button.addEventListener('click', () => createNotification())
-
-function createNotification(message = null, type = null) {
-    const notif = document.createElement('div')
-    notif.classList.add('toast')
-    notif.classList.add(type ? type : getRandomType())
-
-    notif.innerText = message ? message : getRandomMessage()
-
-    toasts.appendChild(notif)
-
-    setTimeout(() => {
-        notif.remove()
-    }, 3000)
-}
-
-function getRandomMessage() {
-    return messages[Math.floor(Math.random() * messages.length)]
-}
-
-function getRandomType() {
-    return types[Math.floor(Math.random() * types.length)]
-}
+form.addEventListener('submit', (e) => {
+})
