156399f2e4861e1428fcccac978e3d2ffd1e9bae
Patrick <Pignataro67@gmail.com>
2025-11-24 11:39:59 -0500

    JS

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ const replay = document.querySelector('#replay')
 runAnimation()
 
 function resetDOM() {
- 
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
+
+  nums[0].classList.add('in')
 }
