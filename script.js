49d93cda8df67e43b2534e4bfcc59ff9da4a181b
Patrick <Pignataro67@gmail.com>
2025-12-21 09:21:13 -0500

    CSS

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
-  
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+    })
 })
