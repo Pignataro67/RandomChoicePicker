27a95f135a84b0a2e52e5e2ff2f35f1a0e5b66df
Patrick <Pignataro67@gmail.com>
2026-01-04 21:25:48 -0500

    JS

---
+++ script.js
@@ -0,0 +1,11 @@
+const range = document.getElementById('range')
+
+range.addEventListener('input', (e) => {
+    const value = +e.target.value
+    const label = e.target.nextElementSibling
+
+    const range_width = getComputedStyle(e.target).getPropertyValue('width')
+    const label_width = getComputedStyle(label).getPropertyValue('width')
+
+    const num_width = +range_width.substring(0, range_width.length - 2)
+})
