9e6dfd91bae658317fadf847c24e0dc44efee68d
Patrick <Pignataro67@gmail.com>
2026-01-14 08:06:46 -0500

    JS

--- script.js
+++ script.js
@@ -21,4 +21,10 @@ const createHeart = (e) => {
     const heart = document.createElement('i')
     heart.classList.add('fas')
     heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
 }
