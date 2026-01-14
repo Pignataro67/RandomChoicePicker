8fb6aa97b947e12edff8d662304514a8b81e782a
Patrick <Pignataro67@gmail.com>
2026-01-14 08:06:56 -0500

    JS

--- script.js
+++ script.js
@@ -27,4 +27,13 @@ const createHeart = (e) => {
 
     const leftOffset = e.target.offsetLeft
     const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
 }
