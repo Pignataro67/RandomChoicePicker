a3fb71e3fd73f5f16bc8c179d9a37e20ffbcb7ea
Patrick <Pignataro67@gmail.com>
2026-01-23 07:14:11 -0500

    JS

--- script.js
+++ script.js
@@ -13,5 +13,12 @@ upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
 
 const changeSlide = (direction) => {
-  
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } 
+    }
 }
