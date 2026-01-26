9bd40d7c17ca900fae14daaa67faf4837aaefbc5
Patrick <Pignataro67@gmail.com>
2026-01-26 08:38:43 -0500

    JS

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
- 
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+const body = document.body
+
+body.addEventListener('dragstart', dragStart)
+body.addEventListener('dragend', dragEnd)
