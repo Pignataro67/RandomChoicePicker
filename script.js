5c670d76022aa67b26013781ae0f5f56772e707b
Patrick <Pignataro67@gmail.com>
2026-02-03 07:56:58 -0500

    JS

--- script.js
+++ script.js
@@ -49,5 +49,10 @@ function drawCircle(x, y) {
 }
 
 function drawLine(x1, y1, x2, y2) {
-    
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
 } 
