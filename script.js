bfbee18618580c30324af8ebae9c4a894489995a
Patrick <Pignataro67@gmail.com>
2026-02-12 07:23:49 -0500

    CSS

---
+++ script.js
@@ -0,0 +1,6 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+updateBigCup()
