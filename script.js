9ec2838c48480856cc72301a9c38f4749cc86e03
Patrick <Pignataro67@gmail.com>
2026-03-27 07:17:05 -0400

    JS

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ async function getUser(username) {
         createUserCard(data)
         getRepos(username)
     } catch(err) {
-         }
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username')
+        }
     }
 } 
