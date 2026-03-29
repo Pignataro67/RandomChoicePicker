d0e95ee140b724e7a8b88e6c204c580e78431ae2
Patrick <Pignataro67@gmail.com>
2026-03-29 09:32:03 -0400

    JS

--- script.js
+++ script.js
@@ -36,7 +36,13 @@ function createUserCard(user) {
       <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
     </div>
     <div class="user-info">
-       
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
 
       <div id="repos"></div>
     </div>
