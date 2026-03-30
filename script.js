a51a1d2cf2a49e539e14a580c39ef31bcfb54501
Patrick <Pignataro67@gmail.com>
2026-03-30 08:31:09 -0400

    JS

--- script.js
+++ script.js
@@ -75,6 +75,11 @@ function addReposToCard(repos) {
 
             reposEl.appendChild(repoEl)
         })
-
-    
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+})
