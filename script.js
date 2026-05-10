c2b51eeea1d2f7e5d95aed825cc57bb9758c43ef
Patrick <Pignataro67@gmail.com>
2026-05-10 07:07:37 -0400

    JS

--- script.js
+++ script.js
@@ -7,4 +7,7 @@ start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', () => {
         const img = btn.querySelector('img')
-    }) 
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selected_insect = { src, alt }
+}) 
