54b553382a134066223472c60b08dd32f34b7a78
Patrick <Pignataro67@gmail.com>
2026-05-12 06:56:30 -0400

    JS

--- script.js
+++ script.js
@@ -40,4 +40,11 @@ function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
     const { x, y } = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style="transform: rotate(${Math.random() * 360}deg)" />`
+
+    insect.addEventListener('click', catchInsect)
+
+    game_container.appendChild(insect)
 } 
