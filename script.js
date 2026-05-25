842f48aeefaa918065d25d778815d23b7c4187cd
Patrick <Pignataro67@gmail.com>
2026-05-25 10:29:44 -0400

    JS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
+
+getData()
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
+ 
