47830ac468919bc71a6ddcfb837f906819531aef
Patrick <Pignataro67@gmail.com>
2026-06-10 07:08:08 -0400

    JS

--- script.js
+++ script.js
@@ -40,5 +40,9 @@ function showMovies(movies) {
 function getClassByRate(vote) {
     if(vote >= 8) {
         return 'green'
-    } 
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 } 
