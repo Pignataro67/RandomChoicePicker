68c2850272d393a7e4794d129f316565b1a2e9ce
Patrick <Pignataro67@gmail.com>
2026-06-18 07:18:52 -0400

    JS

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ const nav = document.querySelectorAll('.nav')
 
 open_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.add('visible'))
-}) 
+})
+
+close_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.remove('visible'))
+})
