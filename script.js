3aebaf458e0d8419dbd60bd4156bf2a751db9034
Patrick <Pignataro67@gmail.com>
2026-07-04 09:34:09 -0400

    JS

--- script.js
+++ script.js
@@ -37,5 +37,8 @@ function generatePassword(lower, upper, number, symbol, length) {
     let generatedPassword = ''
     const typesCount = lower + upper + number + symbol
     const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
- 
+
+    const finalPassword = generatedPassword.slice(0, length)
+
+    return finalPassword
 }
