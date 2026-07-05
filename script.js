7779dcadacaea977bab8d243bc357f4d724932ba
Patrick <Pignataro67@gmail.com>
2026-07-05 10:02:42 -0400

    JS

--- script.js
+++ script.js
@@ -60,4 +60,8 @@ function getRandomLower() {
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}   
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+} 
