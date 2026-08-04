d83a7a21a4d4c1237ef82bb02a436bf5fd5705ac
Patrick <Pignataro67@gmail.com>
2026-08-04 10:13:42 -0400

    JS

--- script.js
+++ script.js
@@ -33,5 +33,13 @@ function randomSelect() {
 
     const interval = setInterval(() => {
         const randomTag = pickRandomTag()
-	 
-    )} 
+	
+	if (randomTag !== undefined) {
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+	}
+    }, 100); 
+} 
