774881f66430343b9c0e53d06993ca53c22451a1
Patrick <Pignataro67@gmail.com>
2026-08-28 09:14:21 -0400

    JS

---
+++ script.js
@@ -0,0 +1,4 @@
+const resetBtn = document.querySelector('#reset');
+const playBtn = document.querySelector('#play');
+const timerEl = document.querySelector('#timer');
+const root = document.querySelector(':root');
