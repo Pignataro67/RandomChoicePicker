347cc2f81ae70205aedb01edb40eb0ee554c87f8
Patrick <Pignataro67@gmail.com>
2026-09-02 06:35:44 -0400

    JS

--- script.js
+++ script.js
@@ -16,5 +16,10 @@ sounds.forEach(sound => {
 })
 
 function stopSongs() {
-     
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
 }
