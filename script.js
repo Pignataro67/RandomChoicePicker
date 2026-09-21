377d355ef3a7fe23e633e9f5f43ce5353be49701
Patrick <Pignataro67@gmail.com>
2026-09-21 08:26:07 -0400

    JS

--- script.js
+++ script.js
@@ -38,5 +38,9 @@ function run() {
 // Format the time
 function formatTime(seconds) {
   const minutes = Math.floor(seconds / 60);
-  const newSeconds = seconds % 60; 
-}
+  const newSeconds = seconds % 60;
+
+  return `${minutes.toString().padStart(2, '0')}:${newSeconds
+    .toString()
+    .padStart(2, '0')}`;
+} 
